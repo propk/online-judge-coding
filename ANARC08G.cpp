af23351df2caeac1c837983a231a77988f861fcb
Pratik Kumar <pratik.kumar.ece12@iitbhu.ac.in>
2014-11-02 00:15:40 +0530

    ANARC08G

--- ANARC08G.cpp
+++ ANARC08G.cpp
@@ -1,14 +1,24 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include<iostream>
 #include<stdio.h>
+#define gc getchar_unlocked // not supported on windows and unsafe
 // Author: Propk
+
+void scanint(int &x) // fast i-o for improved runtime (unsigned i-0)
+{
+    register int c = gc();
+    x = 0;
+    for(;(c<48 || c>57);c = gc());
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+}
 
 using namespace std;
 int arr[1000][1000];
+
 int main() // ad-hoc question
 {
 	int serial, counter = 0;
-	scanf("%d", &serial);
+	scanint(serial);
 	while (serial != 0)
 	{
 		counter++;
@@ -17,7 +27,7 @@ int main() // ad-hoc question
 		{
 			for (int j = 0; j < serial; j++)
 			{
-				scanf("%d", &arr[i][j]);
+				scanint(arr[i][j]);
 				give[j] += arr[i][j];
 				sum2 += arr[i][j];
 			}
@@ -36,7 +46,8 @@ int main() // ad-hoc question
 		int sum = 0;
 		for (int i = 0; i < serial; i++)
 			sum += give[i];
-		cout << counter << ". " << sum2 << " " << sum << endl;
+			printf("%d. %d %d\n",counter,sum2,sum);
+		//cout << counter << ". " << sum2 << " " << sum << endl;
 		scanf("%d", &serial);
 	}//end of test case
 	return 0;
