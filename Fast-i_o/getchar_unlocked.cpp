d44d6a7137ec7587d7dc6086f9f606cd653c0c65
Pratik Kumar <pratik.kumar.ece12@iitbhu.ac.in>
2014-11-02 00:32:59 +0530

    getchar_unlocked

---
+++ Fast-i_o/getchar_unlocked.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<cstdio>
+#define gc getchar_unlocked
+
+void scanint(int &x) // unsigned integer
+{
+    register int c = gc();
+    x = 0;
+    for(;(c<48 || c>57);c = gc());
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+}
+
+void scanint(int &x) // signed integer
+{
+    register int c = gc();
+    x = 0;
+    int neg = 0;
+    for(;((c<48 || c>57) && c != '-');c = gc());
+    if(c=='-') {neg=1;c=gc();}
+    for(;c>47 && c<58;c = gc()) {x = (x<<1) + (x<<3) + c - 48;}
+    if(neg) x=-x;
+}
