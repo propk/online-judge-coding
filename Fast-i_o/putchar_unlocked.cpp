3109a83cf60262858ad6701d294537bc7d5f370d
Pratik Kumar <pratik.kumar.ece12@iitbhu.ac.in>
2014-11-04 01:42:43 +0530

    FAST IO

---
+++ Fast-i_o/putchar_unlocked.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#define pc(x) putchar_unlocked(x);
+
+inline void writeInt (int n)
+{
+    int N = n, rev, count = 0;
+    rev = N;
+    if (N == 0) { pc('0'); pc('\n'); return ;}
+    while ((rev % 10) == 0) { count++; rev /= 10;} //obtain the count of the number of 0s
+    rev = 0;
+    while (N != 0) { rev = (rev<<3) + (rev<<1) + N % 10; N /= 10;}  //store reverse of N in rev
+    while (rev != 0) { pc(rev % 10 + '0'); rev /= 10;}
+    while (count--) pc('0');
+}
